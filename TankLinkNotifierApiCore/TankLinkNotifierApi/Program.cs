b7e90f0e0be4bb604eeb05c2931246b8e9087242
Mark Gillen <mgillen@Telular.Telular.com>
2020-10-15 11:37:56 -0400

    SBTMM2-46 Clean up commented out code

--- TankLinkNotifierApiCore/TankLinkNotifierApi/Program.cs
+++ TankLinkNotifierApiCore/TankLinkNotifierApi/Program.cs
@@ -1,15 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Log4Net;
-//using Serilog;
-//using Serilog.Events;
-//using Serilog.Formatting.Compact;
+
 
 namespace TankLinkNotifierApi
 {
