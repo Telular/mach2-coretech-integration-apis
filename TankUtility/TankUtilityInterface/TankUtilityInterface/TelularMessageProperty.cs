8ef0dead8e5bc2d8f87d8c4ba3707c0ec8f6bc6d
kdontu <kalyani.dontu@ametek.com>
2020-11-12 16:33:37 -0500

    (SBTMTU-8) Create TankUtilty Interface
    (SBTMTU-8) Create TankUtilty Interface

---
+++ TankUtility/TankUtilityInterface/TankUtilityInterface/TelularMessageProperty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceActor.Common.DataContracts
+{
+    public class TelularMessageProperty
+    {
+        public string id { get; set; }
+        public string originationsourcetype { get; set; }
+        public string name { get; set; }
+        public string value { get; set; }
+        public string datatype { get; set; }
+        public string parentsequenceid { get; set; }
+        public string sequenceid { get; set; }
+        public string parentid { get; set; }
+    }
+}
